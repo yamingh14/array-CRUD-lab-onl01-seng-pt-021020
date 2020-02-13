d70d2fd4f3dd3efdbd6d1a6dad07fe0871211843
Yamin Ghowrwal <yamin.gh@gmail.com>
2020-02-13 23:59:24 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -10,7 +10,9 @@ def add_element_to_end_of_array(array, element)
   array.push(element)
 end
 
-def add_element_to_start_of_array
+def add_element_to_start_of_array(array, element)
+  array.unshift(element)
+
 end
 
 

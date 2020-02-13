3d1fcd234a1dc1786c428e8db823abcd21c92ef2
Yamin Ghowrwal <yamin.gh@gmail.com>
2020-02-13 23:57:27 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -6,7 +6,7 @@ def create_an_array
   array = [1, 2, 3, 4]
 end
 
-def add_element_to_end_of_array
+def add_element_to_end_of_array(array, element)
   array = %w[one two three four]
   array.push("five")
 end

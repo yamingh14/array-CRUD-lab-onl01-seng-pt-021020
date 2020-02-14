2d4173695f422516c16196a8d9075fdf38b1ed40
Yamin Ghowrwal <yamin.gh@gmail.com>
2020-02-14 00:07:50 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -27,11 +27,13 @@ def retrieve_element_from_index(array, index_number)
 end
 
 def retrieve_first_element_from_array(array)
-  array[0]
+  # array[0] 
+  array.first
 end
 
 def retrieve_last_element_from_array(array)
-  array[-1]
+  # array[-1]
+  array.last
 end
 
 
